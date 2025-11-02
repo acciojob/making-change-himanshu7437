4d67b809f25ab65ae72e878135a71aafb1a34469
Himanshu Sharma <143724283+himanshu7437@users.noreply.github.com>
2025-11-02 07:32:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
 const makeChange = (c) => {
   // your name here
+	let remaining = c
+
+	const q = Math.floor(remaining/25);
+	remaining = remaining%25;
+
+	const d = Math.floor(remaining/10);
+	remaining = remaining%10;
+
+	const n = Math.floor(remaining/5);
+	remaining = remaining%5;
+
+	const p = remaining
+
+	return {q, d, n, p} 
+	
 };
 
-// Do not the change the code below
+// // Do not the change the code below
 const c = prompt("Enter c: ");
 alert(JSON.stringify(makeChange(c)));
